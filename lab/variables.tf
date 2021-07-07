201f98f0fe9d0f5e31a91c90426f721566892da8
Mselmi <jihed.mselmi@gmail.com>
2021-07-07 16:47:03 +0200

    clean up

--- lab/variables.tf
+++ lab/variables.tf
@@ -13,6 +13,6 @@ variable "node_number" {
 variable "payload" {
   description = "my ssh key payload"
   type = string
-  default = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDQOJzs0gNStUA37uz6hpVWdYJAC7LuH/hU13HUc7IQI/i75g2hEeCRQ4zbo3BIv/K+6MXmYuUGoXfmoHUMEdMoDI4Ai1JJfD5swpuRHt4MUXm7YLw6gcL9NcWwJcYjCI4Ggfftok1eSxVJrIgOIVw8lE/KUcnhPtTgoraBbBV9dzQfmiHRaEhOQCB7nKAhoo183Ks6dXzo850DGh5iqOhQb6uBWM5gd5NuBq1aNqtwkgkaJ3SJwUIzqmHE0nG2PWvZiI9JFPtOOFf/SsQiGh24jdbLikROXqn5BdeoTNYjcQmFJMPXbZhcW5agnIfyWgxHVPeIvDqBSW8e+1yekGzXjX1U/YGwePe/T+Gv1OpE5oLt4nzEWpV1KGsgnq00r6+wTzVY/4LdlOgeYnDErMZx8z/pLbaVah6Sa9ylVnAMS3cV33vCUDjLGM36xW9RzVRFUEB8S6/l14h5+qJZm40WUjSvew/0Vctn1kTDvHW7VDxz3SExpfwQGnm64wnQ/0s= mselj@88665a207c02.ant.amazon.com"
+  default = ""
   
 }
